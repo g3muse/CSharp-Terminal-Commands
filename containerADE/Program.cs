37cbe868010d6355e69e0131fdd5596ef886d43f
g3muse <g3muse@gmail.com>
2017-05-24 21:51:01 -0700

    Add files via upload

---
+++ containerADE/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace containerADE
+{
+    class Program
+    {
+        public static void ExecuteCommand(string command) {
+             Process proc = new System.Diagnostics.Process ();
+            proc.StartInfo.FileName = "/bin/bash";
+            proc.StartInfo.Arguments = "-c \" " + command + " \"";
+            proc.StartInfo.UseShellExecute = false; 
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.Start();
+
+            while (!proc.StandardOutput.EndOfStream) {
+                Console.WriteLine (proc.StandardOutput.ReadLine ());
+            }
+            proc.WaitForExit();
+        }
+    
+        public static void Main (string[] args)
+        {
+            ExecuteCommand("open -a Terminal \"`pwd`\" & ls");
+            ExecuteCommand("docker container ps");
+        }
+
+    }
+
+
+}
